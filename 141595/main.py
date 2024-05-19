3677dd1e192f8019130464aa4136a73e88e7b49b
Pelmeshka08 <dunin-andry@mail.ru>
2024-05-19 18:11:29 +0300

    ludskoe
    здесь людское имя.

---
+++ 141595/main.py
@@ -0,0 +1,58 @@
+from pygame import *
+from random import randint
+
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+#конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+#метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+#класс главного игрока
+class Player(GameSprite):
+    #метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+back = (200, 255, 255) 
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+  
+#класс спрайта-врага 
+class Enemy(GameSprite):
+    #движение врага
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        #исчезает, если дойдёт до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
